99d99d4de0a1d09dda4721b138f3cd7a37d14da4
scheng0718 <scheng0718@gmail.com>
2023-06-15 15:08:30 -0700

    feat: add error handler in app.js

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.set('view engine', 'hbs' )
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Internal Error')
+})
 
 app.listen(3000, () => {
   console.log(`The server is listening at http://localhost:3000`)
