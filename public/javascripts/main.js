17fe08706e16a8e04f3688a96ed4a65e8c4b5117
scheng0718 <scheng0718@gmail.com>
2023-06-14 23:37:18 -0700

    feat: add copy function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('#form')
 const submitButton = document.querySelector('#submitButton')
+const targetUrl = document.querySelector('#target_url')
+const copyBtn = document.querySelector('#copy_btn')
 
 submitButton.addEventListener('click', function onSubmitButtonClicked(event) {
   form.classList.add('was-validated')
@@ -11,3 +13,10 @@ form.addEventListener('submit', function onFormSubmit(event) {
     event.stopPropagation()
   }
 })
+
+function copyUrl() {
+  const url = targetUrl.innerText
+  navigator.clipboard.writeText(url)
+    .then(() => alert('URL Copied!'))
+    .catch(error => console.log(error))
+}
